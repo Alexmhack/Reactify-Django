d7cf8ebcd828c6bcef775ae95b80417b8e4d1bae
alexHooper <alexmohack@gmail.com>
2019-07-29 00:29:05 +0530

    use posts object to get title and content

--- src/reactify-ui/src/posts/PostInline.js
+++ src/reactify-ui/src/posts/PostInline.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 
 class PostInline extends Component {
   render() {
-  	const {title} = this.props
+  	const {post} = this.props
 
     return (
       <div className="App">
-        <h1>{title}</h1>
+        <h1>{post.title}</h1>
+        <p>{post.content}</p>
       </div>
     );
   }
