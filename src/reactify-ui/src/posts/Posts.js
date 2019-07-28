358bb4ae8d05d0029a86e194b2c3b56ba7268b0e
alexHooper <alexmohack@gmail.com>
2019-07-28 20:55:43 +0530

    fetch posts and console log

--- src/reactify-ui/src/posts/Posts.js
+++ src/reactify-ui/src/posts/Posts.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react'
+import 'whatwg-fetch'
+import cookies from 'react-cookies'
 
 class Posts extends Component {
+	loadPosts = () => {
+		const endpoint = '/api/posts/'
+		let lookupOptions = {
+			method: "GET",
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}
+
+		// fetch the posts list api
+		fetch(endpoint, lookupOptions)
+		.then(function(response) {
+			return response.json()
+		}).then(function(responseData) {
+			console.log(responseData)
+		}).catch(function(errors) {
+			console.error(errors)
+		})
+
+	}
+
+	componentDidMount () {
+		this.loadPosts()
+	}
+
 	render () {
 		return (
 			<div>
