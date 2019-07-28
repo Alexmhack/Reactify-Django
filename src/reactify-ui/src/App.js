c6bdc0fb1288bbbd8d6cacf3a44071545e70dca6
alexHooper <alexmohack@gmail.com>
2019-07-28 20:03:55 +0530

    show Posts component

--- src/reactify-ui/src/App.js
+++ src/reactify-ui/src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Posts from './posts/Posts'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          This is the working react &amp; django integration.
-        </p>
+        <Posts />
       </div>
     );
   }
