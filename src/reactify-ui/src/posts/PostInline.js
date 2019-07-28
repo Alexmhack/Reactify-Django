2558df4965d2bc721dd3404ce52d54bc0f5e1215
alexHooper <alexmohack@gmail.com>
2019-07-29 00:02:28 +0530

    use props to get title

--- src/reactify-ui/src/posts/PostInline.js
+++ src/reactify-ui/src/posts/PostInline.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react'
 
 class PostInline extends Component {
   render() {
+  	const {title} = this.props
+
     return (
       <div className="App">
         <p>Posts are now rendered inline.</p>
+        <h1>{title}</h1>
       </div>
     );
   }
