2e6a1486f07532d7eb93f99907337d3107f938fe
alexHooper <alexmohack@gmail.com>
2019-07-28 23:54:49 +0530

    create component for inline posts

--- src/reactify-ui/src/posts/PostInline.js
+++ src/reactify-ui/src/posts/PostInline.js
@@ -1,1 +1,13 @@
+import React, { Component } from 'react'
 
+class PostInline extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Posts are now rendered inline.</p>
+      </div>
+    );
+  }
+}
+
+export default PostInline
