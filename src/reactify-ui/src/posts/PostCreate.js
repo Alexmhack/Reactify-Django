8ef58f12bdc1b810855f93508f2ddfc90b6dd435
alexHooper <alexmohack@gmail.com>
2019-07-30 16:07:19 +0530

    add form to create post

--- src/reactify-ui/src/posts/PostCreate.js
+++ src/reactify-ui/src/posts/PostCreate.js
@@ -3,9 +3,26 @@ import React, { Component } from 'react'
 class PostCreate extends Component {
 	render () {
 		return (
-			<div>
-
-			</div>
+			<form>
+				<div className='form-group'>
+					<label for='id_name'>Post Title</label>
+					<input type='text' id='id_name' name='title' className='form-control' placeholder='Blog post title' />
+				</div>
+				<div className='form-group'>
+					<label for='id_content'>Post Content</label>
+					<textarea type='text' id='id_content' name='content' className='form-control' placeholder='Blog post content'>
+					</textarea>
+				</div>
+				<div className='form-group'>
+					<input type='checkbox' id='id_draft' name='draft' className='form-control' />
+					<label for='id_draft'>Draft</label>
+				</div>
+				<div className='form-group'>
+					<label for='id_publish_date'>Publish date</label>
+					<input type='date' id='id_publish_date' name='publish' className='form-control' />
+				</div>
+				<button className='btn btn-primary'>Save</button>
+			</form>
 		)
 	}
 }
