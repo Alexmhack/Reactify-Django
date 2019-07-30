32ac9bda8e63bd002e88fdaec4c9d65372257143
alexHooper <alexmohack@gmail.com>
2019-07-30 18:01:00 +0530

    set input values in state

--- src/reactify-ui/src/posts/PostCreate.js
+++ src/reactify-ui/src/posts/PostCreate.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react'
 
 class PostCreate extends Component {
+	constructor (props) {
+		super(props)
+		this.state = {
+
+		}
+	}
+
 	handleSubmit = (event) => {
 		event.preventDefault()
+	}
+
+	handleInputChange = (event) => {
+		event.preventDefault()
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+		console.log(this.state)
 	}
 
 	render () {
@@ -10,20 +25,24 @@ class PostCreate extends Component {
 			<form action='.' method='POST' onSubmit={this.handleSubmit}>
 				<div className='form-group'>
 					<label for='id_name'>Post Title</label>
-					<input type='text' id='id_name' name='title' className='form-control' placeholder='Blog post title' />
+					<input type='text' id='id_name' name='title' className='form-control'
+						placeholder='Blog post title' onChange={this.handleInputChange} />
 				</div>
 				<div className='form-group'>
 					<label for='id_content'>Post Content</label>
-					<textarea type='text' id='id_content' name='content' className='form-control' placeholder='Blog post content'>
+					<textarea type='text' id='id_content' name='content' className='form-control'
+						placeholder='Blog post content' onChange={this.handleInputChange}>
 					</textarea>
 				</div>
 				<div className='form-group'>
-					<input type='checkbox' id='id_draft' name='draft' className='form-control' />
+					<input type='checkbox' id='id_draft' name='draft' className='form-control'
+						 onChange={this.handleInputChange} />
 					<label for='id_draft'>Draft</label>
 				</div>
 				<div className='form-group'>
 					<label for='id_publish_date'>Publish date</label>
-					<input type='date' id='id_publish_date' name='publish' className='form-control' />
+					<input type='date' id='id_publish_date' name='publish' className='form-control'
+						 onChange={this.handleInputChange} />
 				</div>
 				<button type='submit' className='btn btn-primary'>Save</button>
 			</form>
