d0ae74c63e5329c224847a4f616abeb7346f3cf9
alexHooper <alexmohack@gmail.com>
2019-07-31 18:19:15 +0530

    use re_path for posts and react

--- src/reactify/urls.py
+++ src/reactify/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.views.generic import TemplateView
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='react.html')),
     path('admin/', admin.site.urls),
+    re_path(r'^posts/', TemplateView.as_view(template_name='react.html')),
     path('api/posts/', include('posts.urls'))
 ]
