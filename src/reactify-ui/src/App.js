bd819a10b05fe058f992fe8576a5a5a98bea96f7
alexHooper <alexmohack@gmail.com>
2019-07-31 18:18:00 +0530

    use router-dom for posts

--- src/reactify-ui/src/App.js
+++ src/reactify-ui/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Posts from './posts/Posts'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Posts />
+      	<BrowserRouter>
+      		<Switch>
+        		<Route component={Posts} />
+        	</Switch>
+        </BrowserRouter>
       </div>
     );
   }
