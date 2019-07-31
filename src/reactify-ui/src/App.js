9344681de5e9d142d0b030d446f7e26c161c2699
alexHooper <alexmohack@gmail.com>
2019-07-31 18:30:49 +0530

    use route to match url to component

--- src/reactify-ui/src/App.js
+++ src/reactify-ui/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Posts from './posts/Posts'
+import PostDetail from './posts/PostDetail'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 class App extends Component {
@@ -8,6 +9,8 @@ class App extends Component {
       <div className="App">
       	<BrowserRouter>
       		<Switch>
+        		<Route exact path='/posts/' component={Posts} />
+        		<Route exact path='/posts/:slug/' component={PostDetail} />
         		<Route component={Posts} />
         	</Switch>
         </BrowserRouter>
